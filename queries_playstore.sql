8c847ef51fcd2230da3e7cfe9b8d424bbfb409fc
Eric Moore <ericmoore0709@gmail.com>
2024-04-10 01:17:24 -0700

    2024 04 10 01:17

--- queries_playstore.sql
+++ queries_playstore.sql
@@ -1,1 +1,5 @@
 -- Comments in SQL Start with dash-dash --
+select * from analytics where id = 1880;
+select id, app_name from analytics where last_updated = "2018-08-01";
+select concat(category, ' - ', count(*)) from analytics group by category;
+select * from analytics -- Find the top 5 most-reviewed apps and the number of reviews for each.
--- queries_products.sql
+++ queries_products.sql
@@ -1,1 +1,22 @@
 -- Comments in SQL Start with dash-dash --
+insert into products (name, price, can_be_returned) values ("chair", 44.00, false);
+insert into products (name, price, can_be_returned) values ("stool", 25.99, true);
+insert into products (name, price, can_be_returned) values ("table", 124.00, false);
+
+select * from products;
+select name from products;
+select name, price from products;
+
+insert into products (name. price, can_be_returned) values ("nyProduct", 1.00, false);
+
+select * from products where can_be_returned = true;
+select * from products where price < 44.00;
+select * from products where price > 22.50 and price < 99.99;
+
+update products set price = price - 20.00;
+
+delete from products where price < 25.00;
+
+update products set price = price + 20.00;
+
+update products set can_be_returned = true;
